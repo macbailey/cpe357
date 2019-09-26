fbf5dd962bdac4a055e26db64b897a1cc930a590
Matthew Bailey <baileymatt57@gmail.com>
2019-09-26 16:22:36 -0700

    I think we are good to go

---
+++ assn1/IOTestCase.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+	int c; 
+	int count = 0; 
+	int spaces; 
+	while ((c = getchar()) != EOF)
+	{
+		switch(c)
+		{
+			case '\t' : 
+				spaces = 8 - count % 8;
+				int i; 
+				for(i = 0; i < spaces; i = i +1)
+				{
+					putchar(' ');
+				}
+				break;
+			case '\b' :
+				if(count >=  1)
+				{
+					count = count - 1;
+				}
+				putchar(c);  
+				break; 
+			case '\n' :
+			case '\r' :
+				count = 0;
+				putchar(c); 
+				break; 
+			default:
+				count++;
+				putchar(c); 
+				break;
+		}
+	}
+	return(0);
+}
