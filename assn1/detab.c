e72da190bef72d7ed5b9eb3d0cde01322fdf1948
Matthew Bailey <baileymatt57@gmail.com>
2019-09-30 13:28:22 -0700

    lab02 has begun

--- assn1/detab.c
+++ assn1/detab.c
@@ -2,34 +2,42 @@
 
 int main()
 {
-	int c; 
-	int count = 0; 
-	int spaces; 
+	int c;		 //Switch statement int  
+	int count = 0;	 //Count for what position the cursor is in 
+	int spaces;	 //Holds the position to the next tab bar
+	//While not at the end of the file 
 	while ((c = getchar()) != EOF)
-	{
+	{	
+		//Switches through 4 cases
 		switch(c)
 		{
+			//Case: Tab
 			case '\t' : 
+				//Calculates how many spaces to next tab bar
 				spaces = 8 - count % 8;
 				int i; 
+				//Fill position to next tab bar with spaces 
 				for(i = 0; i < spaces; i++)
 				{
 					putchar(' ');
 					count++; 
 				}
 				break;
+			//Case: Backspace
 			case '\b' :
 				if(count > 0)
 				{
-					count = count - 1;
+					count = count - 1; //Go one position back
 				}
 				putchar(c);  
 				break; 
+			//Case: Newline & Carriage Return
 			case '\n' :
 			case '\r' :
 				count = 0; 
 				putchar(c); 
 				break; 
+			//Default: print character and increment the position by one 
 			default:
 				putchar(c);
 				count++; 
