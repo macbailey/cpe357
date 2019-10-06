cac197be361ead1868341b8fa76ae9e7fb2294e5
macbailey <baileymatt57@gmail.com>
2019-10-05 22:17:20 -0700

    file is good learn how to load inot memory:

--- lab02/FileOpener.c
+++ lab02/FileOpener.c
@@ -1,39 +1,19 @@
 #include <stdio.h>
 
-main() {
-
+int main(int argc, char *argv[]) 
+{
 	int c; 
-	/* char ch; */ 
    FILE *fp;
-	fp = fopen("in_test.txt", "r");
+	fp = fopen("in_test.txt", "r+");
    if (fp != NULL)
    {
 	   while ((c = fgetc((FILE*)fp)) != '\n')
 	   {
-	   	fputc(c, (FILE*)fp);
-	   	
+	   	fputc(c, stdout);
 	   }
+	   printf("\n");
 	   fclose(fp);
 	   return 0; 
    }
-  return 1; 
-   
+	return 1; 
 }
-
-
-/* int main()
-{
-   char ch;
-   FILE *fp;
-   if (fp = fopen("test.c", "r"))
-   {
-     ch = getc(fp);
-     while (ch != EOF)
-     {
-        putc(ch, stdout);
-        ch = getc(fp);
-     }
-     fclose(fp);
-     return 0;
-   }
-*/
