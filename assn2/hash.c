97827773578de0dfe747bd993788d8b7debe8a31
Matt Bailey <baileymatt57@gmail.com>
2019-10-10 16:49:43 -0700

    new stuff

---
+++ assn2/hash.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TABLE_SIZE = 30000;
+
+typedef struct Node { 
+	int key; 
+	char value; 
+	int count = 0; 
+}
+int main(int argc, char *argv[])
+{
+	File *fp = fopen(argv[1], "r+"); 
+
+	if (fp == NULL)
+	{
+		printf("There doesn't seem to be anything here"); 
+	}
+}
+
+void insert(int key, char value)
+{
+	struct Node *entry = (struct Node*)
+						malloc(sizeof(struct Node));
+	/*key will be the string name horner hashed*/
+	entry->key = horner_hash(value);
+	entry->value = value; 
+	 
+}
+
