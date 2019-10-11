be85cd474578a36136fda841683a6026a2db460b
Matt Bailey <baileymatt57@gmail.com>
2019-10-10 17:38:07 -0700

    update for allen

--- assn2/hash.c
+++ assn2/hash.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define TABLE_SIZE = 30000;
+#define TABLE_SIZE = 3000;
 
 typedef struct Node { 
 	int key; 
 	char value; 
 	int count = 0; 
 }
+int num_items = 0; 
 int main(int argc, char *argv[])
 {
 	File *fp = fopen(argv[1], "r+"); 
@@ -35,8 +36,28 @@ void insert(int key, char value)
 
 	if(table[entry->key] == value)
 	{
-		entry->count
+		entry->count++;
 	}
+
+	num_items++; 
 	 
 }
+
+int horner_hash(char value)
+{
+	int i = 0; 
+	if(len(value) > 8)
+	{
+		n = 8; 
+	} else {
+		n = len(value)
+	}
+	while (i < len(value))
+	{
+		int key += ord(value[i])*31**(n-1-i);
+	}
+	key = key % table_size();
+}
+
+int 
 
