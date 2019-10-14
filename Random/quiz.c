56172dee9660231bb6e99c36638d2922447ad87c
mbaile17 <baileymatt57@gmail.com>
2019-10-14 10:28:29 -0700

    update

--- Random/quiz.c
+++ Random/quiz.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
-int main()
-{	int a = 2; int b = -1; 
-	int result = a++ + ++b; 
-	printf("%d\n", result); 
-	return 0; }
-	
+#include <string.h>
+
+size_t strsp(const char *s, const char *accept)
+{
+	int i, j = 0; 
+	size_t count = 0; 
+
+	while(i < strlen(accept))
+	{
+		while(j < strlen(s))
+		{
+			if(s[j] == accept[i])
+			{
+				count++;
+			}
+			j++;		
+		}
+		j = 0;
+
+		i++;
+	}
+
+	if(count == 0)
+	{
+		return -1;
+	}else{
+		return count;
+	}
+}
+
+int main () {
+   int len;
+   const char str1[] = "ABCDEFG019874";
+   const char str2[] = "ABCD";
+
+   len = strsp(str1, str2);
+
+   printf("Length of initial segment matching %d\n", len );
+   
+   return(0);
+}
+
+
