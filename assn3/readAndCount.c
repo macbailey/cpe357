dadc073cbcad14259c272fb5785d4e9627496696
Matt Bailey <baileymatt57@gmail.com>
2019-10-16 19:21:29 -0700

    he

--- assn3/readAndCount.c
+++ assn3/readAndCount.c
@@ -2,28 +2,49 @@
 #include <stdio.h>
 #include <string.h>
 #define MAX_COUNT 256
+
+struct node{
+	char *name; 
+	int count; 
+	struct node *next; 
+}; 
+
 int main(int argc, char* argv[])
 {
 	char c;
 	int count = 0; 
-	int freq_Counter[MAX_COUNT]; 
+	struct node *freq_Counter = malloc(sizeof(struct node)*MAX_COUNT);
 	FILE *infile = fopen(argv[1], "r");
 	if(!infile)
 		perror(argv[1]);
 
 	while(count < MAX_COUNT)
 	{
-		freq_Counter[count] = 0; 
-		count ++;
+		freq_Counter[count].name = (char *)count; 
+		freq_Counter[count].count = 0; 
+		count++; 
 	}
+	count = 0; 
 	while((c = fgetc(infile)) != EOF)
 	{
 /*		if(c == NULL)
 			perror(c); */
-		freq_Counter[(int)c]++; 
+		freq_Counter[(int)c].count++;  
 
 	}
+	while(count < MAX_COUNT)
+	{
+		if(count == 10)
+		{
+			printf("%d (nl) ", freq_Counter[count].name); 
+			printf("%d\n", freq_Counter[count].count);
+		}
+		if(freq_Counter[count].count != 0 && count != 10)
+		{
+			printf("%d (%c) ",freq_Counter[count].name, freq_Counter[count].name);
+			printf("%d\n", freq_Counter[count].count);
+		}
+		count++; 
+	}
 	return 0; 
-
-
 }
