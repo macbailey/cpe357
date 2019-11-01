e7e8768b116ffb72e5fb8fecbabde11c7c4d0047
mbaile17 <baileymatt57@gmail.com>
2019-11-01 12:04:08 -0700

    update

--- lab05/mypwd.c
+++ lab05/mypwd.c
@@ -4,19 +4,25 @@
 #include <string.h>
 #include <limits.h>
 #include <dirent.h>
+#include<unistd.h>  
+#define MAX_PATH 2048
 
-int dirFinder(char* path, int level)
+int dirFinder(char** path, int level)
 { 
   DIR *prevDir; 
   struct dirent* currectDir; 
+  struct stat here;
+  struct stat there;
+
+
 
   
   /*open up previous directory*/
   prevDir = opendir("..");
-
+  /*Check if prev*/
   if(prevDir == NULL)
   {
-    perror("mypwd");
+    perror("cannot get current directory");
     return 1; 
   }
   if(level >= MAX_PATH)
@@ -24,21 +30,36 @@ int dirFinder(char* path, int level)
     perror("path too long");
     return 2; 
   }
+  stat(".", &here);
+  chdir("..");
+  /*Reads through whole directory*/
   while((currectDir = readdir(prevDir)) != NULL)
   {
+    stat(currectDir->d_name, &there);
 
-    printf("currectDir:\n");
+/*    printf("Here: %i  %i  \n", here.st_dev, here.st_ino);
+    printf("There: %i  %i  \n", there.st_dev, there.st_ino);  */
+    if(there.st_dev == here.st_dev && there.st_ino == here.st_ino)
+    {
+      printf("currectDir: %s\n", currectDir->d_name);
+      path[level] = currectDir->d_name; 
+      level++; 
+      dirFinder(path, level);
+
+    }     
+    
   }
+  return 0; 
 }
 
 int main(int argc, char* argv[])
 {
-  char* path;
-  int level;
-  int retVal = 0; 
+  char** path;
+  int level = 0;
+  int retVal; 
 
-  path = malloc(sizeof(path)*MAX_PATH)
-  retVal = dirFinder(path, level)
+  path = malloc(sizeof(path)*MAX_PATH);
+  retVal = dirFinder(path, level);
 
   return retVal; 
 }
