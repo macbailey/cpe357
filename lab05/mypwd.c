28287dc0b0c34148401ca7ae4fef5fc631345fd3
Matthew Bailey <baileymatt57@gmail.com>
2019-11-11 15:55:03 -0800

    adding tar file

--- lab05/mypwd.c
+++ lab05/mypwd.c
@@ -18,54 +18,75 @@ int dirFinder(char** path, int level)
   
   /*open up previous directory*/
   prevDir = opendir("..");
-  /*Check if prev*/
+  /*Check if prev is not NULL */
   if(prevDir == NULL)
   {
     perror("cannot get current directory");
     return 1; 
   }
+  /*If the path is beyond 2048*/
   if(level >= MAX_PATH)
   {
     perror("path too long");
     return 2; 
   }
+
+  /*To check if current and parent are same*/
   stat(".", &checkHere);
   stat("..", &checkThere);
-  if(checkThere.st_ino == checkHere.st_ino)
+  if(checkThere.st_ino == checkHere.st_ino && 
+    checkThere.st_dev == checkHere.st_dev)
   {
     closedir(prevDir);
-    return 0; 
+    return level;
   }
+
+  /*set a structure for the local directory*/
   stat(".", &here);
   chdir("..");
   /*Reads through whole directory*/
   while((currectDir = readdir(prevDir)) != NULL)
   {
+    /*setting directory name*/
     stat(currectDir->d_name, &there);
-
-/*    printf("Here: %i  %i  \n", here.st_dev, here.st_ino);
-    printf("There: %i  %i  \n", there.st_dev, there.st_ino);  */
+    /*If the directory here matches there*/
     if(there.st_dev == here.st_dev && there.st_ino == here.st_ino)
     {
-      printf("currectDir: %s\n", currectDir->d_name);
-      path[level] = currectDir->d_name; 
-      level++; 
-      dirFinder(path, level);
-
-    }     
-    
+      path[level] = currectDir->d_name;  
+      /*Recursive call*/
+      dirFinder(path, ++level);
+      break;
+    }       
   }
   return 0; 
 }
 
 int main(int argc, char* argv[])
 {
-  char** path;
+  char** path = malloc(sizeof(path)*MAX_PATH);
+  char* out_path = malloc(sizeof(out_path)*MAX_PATH); 
   int level = 0;
-  int retVal; 
+  int i; 
 
-  path = malloc(sizeof(path)*MAX_PATH);
-  retVal = dirFinder(path, level);
+  out_path = '\0';
 
-  return retVal; 
+  dirFinder(path, level);
+
+  /*Find length of path*/
+  for(i = 0; i < MAX_PATH; i++)
+  {
+    if(path[i] != NULL)
+    {
+      level++; 
+    }
+  }
+
+  /*print out path*/
+  printf("/");
+  for(i = level-1; i > -1; i--)
+  {
+    printf("/%s", path[i]);
+  }
+  printf("\n");
+  return 0; 
 }
