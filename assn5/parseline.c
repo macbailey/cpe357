ecee261b7d5d5725859d6150c04cb4b019f1dd0a
Matt Bailey <baileymatt57@gmail.com>
2019-11-18 12:52:33 -0800

    heading to server

--- assn5/parseline.c
+++ assn5/parseline.c
@@ -1,7 +1,49 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void removeSpaces(char* input, char** output)
+{
+  const char space[2] = " "; 
+  char *token; 
+  
+  token = strtok(input, space); 
+  while(token != NULL)
+  {
+    if(strcmp(token, " ") == 0)
+      continue; 
+    token = *output; 
+    ++*output;
+    token = strtok(NULL, space); 
+  }
+}
+
+/*  for(i = 0; i < strlen(input); i++)
+  {
+    if(*input == ' ')
+    {
+      ++input;
+      if(output[0] != NULL)
+      {
+        *++output; 
+      }
+    }
+    else{
+      strcat(*output, input);
+    }
+  }
+}*/
+
 int main(int argc, char *argv[])
 {
+  int i; 
   char input[512] = {'\0'};
-  printf("Tell me something\n");
-  sscanf("%s", input); 
-  printf("%s\n", input);
+  char *comms_args[100]; 
+  printf("Tell me something: \n");
+  fgets(input, 512, stdin); 
+  removeSpaces(input, comms_args);
+  for(i = 0; i < 99; i++)
+  {
+    printf("output %i: %s \n", i, comms_args[i]);
+  }
 }
