3e70ebdd740db3d982d30a733de0334ea8e06670
Matt Bailey <baileymatt57@gmail.com>
2019-12-01 18:31:09 -0800

    adding lab07

---
+++ lab07/check_fork.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+
+int main() 
+{ 
+  int pid;
+
+  pid = fork(); 
+
+  if (pid == 0) {  
+
+    printf("This is the child, pid  %d\n", 
+      getpid()); 
+  } 
+
+  else
+  {  
+    printf("This is the parent, pid %d\n", getpid()); 
+    wait(NULL);
+  } 
+  return 0; 
+}
